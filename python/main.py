65e2948dd5da57fe0465945ba06e32cc9bd7aa52
yashasnadigsyn <yashasnadigsyn@proton.me>
2023-01-28 20:43:59 +0530

    first message

--- python/main.py
+++ python/main.py
@@ -9,9 +9,8 @@ def ipfsgur():
 
     #getting tags
     payload = request.form.to_dict()
-    name_of_image = payload['name']
     public = payload['public']
-    print(public, name_of_image)
+    print(public)
 
     #reading file without saving using PIL library
     file = request.files['image']
@@ -20,16 +19,34 @@ def ipfsgur():
         token = f.read()
 
     #uploading to web3.storage
-    w3 = w3storage.API(token=token)
+    w3 = w3storage.API(token=token.strip())
 
     #converting image to temporary bytes and uploading it to web3.storage
     image_bytes = io.BytesIO()
     img.save(image_bytes, format=img.format)
     image_bytes = image_bytes.getvalue()
-    imageID = w3.post_upload((name_of_image, image_bytes))
+    imageID = w3.post_upload((image_bytes))
+
+    if str(public).lower() == "true":
+        with open('public_cids.txt', 'a') as f:
+            f.write(f"{imageID}\n")
 
     #returning CID
-    return jsonify({'msg':'success', 'Content_ID': imageID})
+    return jsonify({'Content_ID': imageID})
+
+@app.route('/images', methods=['GET'])
+def getimages():
+    with open('token.txt', 'r') as f:
+        token = f.read()
+    w3 = w3storage.API(token=token.strip())
+    all_images = w3.user_uploads()
+    public_images = []
+    with open('public_cids.txt', 'r') as f:
+        public_cids = f.readlines()
+    for i in all_images:
+        if f'{i["cid"]}\n' in public_cids:
+            public_images.append(i['cid'])
+    return public_images
 
 if __name__ == "__main__":
     app.run(debug=True, port=8200)
